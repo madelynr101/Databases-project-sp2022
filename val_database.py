beb99276774e6881af0f2939468283f26b6825d2
Madelyn <mrwptd@umsystem.edu>
2022-04-25 16:51:27 -0500

    basic additions

--- val_database.py
+++ val_database.py
@@ -1,3 +1,72 @@
 #this is a databse 
-
 import os
+
+
+
+
+# ---------- functions -----------------
+
+# Search for games based on certain maps
+def games_by_map(map_name):
+games[] = query all games with the same map_name
+print(games[])
+
+# Search for game that were won by certain teams
+def games_won_by(team_name):
+games[] = query all games won by team_name
+print(games[])
+
+# Search for games that were a part of certain tournaments
+def tourna_games(tournament_name)
+games[] = query all games with tournament_name
+print(games[])
+
+# Search for games by Gold/Green MST team 
+def MST(team_name)
+games[] = query all games with team_name in it
+print(games[])
+ 
+# In the searched games above be able to find rank 
+def rank(game, IGN)
+query game for rank if they match IGN
+print(rank)
+
+# In the searched games above be able to find agent
+def agent(game, IGN)
+query game for rank if they match IGN
+print(agent)
+
+# In the searched games above be able to find KDA
+def KDA(game, IGN)
+query game for rank if they match IGN
+print(KDA)
+
+# In the searched games above be able to find role 
+def role(game, IGN)
+query game for rank if they match IGN
+print(role)
+
+# In the searched games above be able to find RSV
+def RSV(game, IGN)
+query game for rank if they match IGN
+print(RSV)
+
+# look at last entry in database’s game number then print that number
+def total_games() 
+
+# prompt for all aspects of a game then add it into database
+def add_game()  
+
+def main() -> int:
+  Team_name = input("What is your team name?")
+  Winning_team = input("What is the name of the winning team?")
+  Map = input("What map did you play?")
+  MST_final_score = int(input("what was the MST final score?"))
+  Other_team_final_score = int(input("What was the score of the opposing team?"))
+  Tournament = input("What tournament was this?")
+  Num_games = int(input("How many games were played?"))
+  Game_num = int(input("What game number are you currently on?")) #IDK IF THIS ONE IS CORRECT
+  Team_name = input("What is the name of  team")
+
+if __name__ == '__main__':
+    sys.exit(main()) 
