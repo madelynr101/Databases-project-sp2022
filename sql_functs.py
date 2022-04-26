6b6e13d3c436bab6af8eba32acb830ff7df62565
jckk54 <jckk54@umsystem.edu>
2022-04-26 18:52:06 -0500

    moved funct pseudo code to  sql_functs

--- sql_functs.py
+++ sql_functs.py
@@ -1,6 +1,79 @@
-<<<<<<< HEAD
 import os, sys
 import sqlite3
 
 from tabulate import tabulate
 
+# Search for games based on certain maps
+def games_by_map(db_file, map_name):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+games[] = query all games with the same map_name
+print(games[])
+
+# Search for game that were won by certain teams
+def games_won_by(db_file, team_name):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+games[] = query all games won by team_name
+print(games[])
+
+# Search for games that were a part of certain tournaments
+def tourna_games(db_file, tournament_name):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+games[] = query all games with tournament_name
+print(games[])
+ 
+# In the searched games above be able to find rank 
+def rank(db_file, game, IGN):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+query game for rank if they match IGN
+print(rank)
+
+# In the searched games above be able to find agent
+def agent(db_file, game, IGN):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+query game for rank if they match IGN
+print(agent)
+
+# In the searched games above be able to find KDA
+def KDA(db_file, game, IGN):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+query game for rank if they match IGN
+print(KDA)
+
+# In the searched games above be able to find role 
+def role(db_file, game, IGN):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+query game for rank if they match IGN
+print(role)
+
+# In the searched games above be able to find RSV
+def RSV(db_file, game, IGN):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+query game for rank if they match IGN
+print(RSV)
+
+# look at last entry in database’s game number then print that number
+def total_games(db_file): 
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
+# prompt for all aspects of a game then add it into database
+def add_game(db_file):
+    with sqlite3.connect(db_file) as connector:
+        curs = connector.cursor()
+        s1 = "select "
--- val_database.py
+++ val_database.py
@@ -1,82 +1,36 @@
 # this is a databse 
-import os
+import os, sys
+import sqlite3
+from getpass import getpass
 
+import sql_functs
 
+DB_FILE = "val.db"
+PROMPT = "sus"
 
-
-# ---------- functions -----------------
-
-# Search for games based on certain maps
-def games_by_map(map_name):
-games[] = query all games with the same map_name
-print(games[])
-
-# Search for game that were won by certain teams
-def games_won_by(team_name):
-games[] = query all games won by team_name
-print(games[])
-
-# Search for games that were a part of certain tournaments
-def tourna_games(tournament_name):
-games[] = query all games with tournament_name
-print(games[])
-
-# Search for games by Gold/Green MST team 
-def MST(team_name):
-games[] = query all games with team_name in it
-print(games[])
- 
-# In the searched games above be able to find rank 
-def rank(game, IGN):
-query game for rank if they match IGN
-print(rank)
-
-# In the searched games above be able to find agent
-def agent(game, IGN):
-query game for rank if they match IGN
-print(agent)
-
-# In the searched games above be able to find KDA
-def KDA(game, IGN):
-query game for rank if they match IGN
-print(KDA)
-
-# In the searched games above be able to find role 
-def role(game, IGN):
-query game for rank if they match IGN
-print(role)
-
-# In the searched games above be able to find RSV
-def RSV(game, IGN):
-query game for rank if they match IGN
-print(RSV)
-
-# look at last entry in database’s game number then print that number
-def total_games(): 
-
-# prompt for all aspects of a game then add it into database
-def add_game():  
+def clear_term():
+    print(chr(27) + "[2J")
 
 def main_menu():
-  Team_name = input("What is the team name?")
-  Winning_team = input("What is the name of the winning team?")
-  Map = input("What map did you play?")
-  MST_final_score = int(input("what was the MST final score?"))
-  Other_team_final_score = int(input("What was the score of the opposing team?"))
-  Tournament = input("What tournament was this?")
-  #Game_num = int(input("Give me a number that hasn't been given yet")) #might add var to use this
-  First = input("What is the manager's first name?")
-  Last = input("What is the manager's last name?")
-  KPR = int(input("What are the KPRs?"))
-  Rounds_survived = int(input("How many rounds were survived?"))
-  Kills = int(input("How many kills?"))
-  Deaths = int(input("How many deaths?"))
-  Assists = int(input("How many assists?"))
-  Agent = input("What agent?")
-  IGN = input("Who is the IGN?")
-  Rank = input("What is the rank?")
-  IRL_name = input("What is their IRL name?")
-  Role = input("What is their role?")
+    Team_name = input("What is the team name?")
+    Winning_team = input("What is the name of the winning team?")
+    Map = input("What map did you play?")
+    MST_final_score = int(input("what was the MST final score?"))
+    Other_team_final_score = int(input("What was the score of the opposing team?"))
+    Tournament = input("What tournament was this?")
+    #Game_num = int(input("Give me a number that hasn't been given yet")) #might add var to use this
+    First = input("What is the manager's first name?")
+    Last = input("What is the manager's last name?")
+    KPR = int(input("What are the KPRs?"))
+    Rounds_survived = int(input("How many rounds were survived?"))
+    Kills = int(input("How many kills?"))
+    Deaths = int(input("How many deaths?"))
+    Assists = int(input("How many assists?"))
+    Agent = input("What agent?")
+    IGN = input("Who is the IGN?")
+    Rank = input("What is the rank?")
+    IRL_name = input("What is their IRL name?")
+    Role = input("What is their role?")
 
 
 if __name__ == '__main__':
